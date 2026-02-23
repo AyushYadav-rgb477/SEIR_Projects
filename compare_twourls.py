5fc0063a1087a24bde96606844453dccab053001
Ayush Yadav <ayushk22824@gmail.com>
2026-02-23 23:20:07 +0530

    Implement SimHash-based webpage similarity detection using 64-bit rolling hash
    Similarity between two urls

--- compare_twourls.py
+++ compare_twourls.py
@@ -1,51 +1,73 @@
 import requests
 import argparse
-import re
-from collections import Counter
 from bs4 import BeautifulSoup
 def get_text(url):
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, "html.parser")
-    for tag in soup(["script", "style", "noscript"]):
-        tag.decompose()
+    r=requests.get(url)
+    soup=BeautifulSoup(r.text,"html.parser")
+    tags=soup(["script","style","noscript"])
+    for t in tags:
+        t.decompose()
     return soup.get_text(" ")
-def word_freq(text):
-    words = re.findall(r"[A-Za-z0-9]+", text.lower())
-    return Counter(words)
+def build_frequency(text):
+    frq={}
+    word=""
+    text=text.lower()
+    for ch in text:
+        if ch.isalnum():
+            word+=ch
+        else:
+            if word!="":
+                if word in frq:
+                    frq[word]+=1
+                else:
+                    frq[word]=1
+                word=""
+    if word!="":
+        if word in frq:
+            frq[word]+=1
+        else:
+            frq[word]=1
+    return frq
 def hash64(word):
-    p = 53
-    m = 2**64
-    h = 0
-    power = 1
+    p=53
+    m=2**64
+    h=0
+    power=1
     for c in word:
-        h = (h + ord(c) * power) % m
-        power = (power * p) % m
+        h=(h+ord(c)*power)%m
+        power=(power*p)%m
     return h
-def simhash(counts):
-    v = [0] * 64
-    for word, cnt in counts.items():
-        h = hash64(word)
+def simhash(freq):
+    v=[0]*64
+    for word in freq:
+        cnt=freq[word]
+        h=hash64(word)
         for i in range(64):
-            if (h >> i) & 1:
-                v[i] += cnt
+            bit=(h>>i)&1
+            if bit==1:
+                v[i]+=cnt
             else:
-                v[i] -= cnt
-    fp = 0
+                v[i]-=cnt
+    fingerprint=0
     for i in range(64):
-        if v[i] > 0:
-            fp |= (1 << i)
-    return fp
-def common_bits(h1, h2):
-    diff = bin(h1 ^ h2).count("1")
-    return 64 - diff
-parser = argparse.ArgumentParser()
+        if v[i]>0:
+            fingerprint|=(1<<i)
+    return fingerprint
+def common_bits(h1,h2):
+    x=h1^h2
+    diff=0
+    while x:
+        diff+=x&1
+        x>>=1
+    return 64-diff
+parser=argparse.ArgumentParser()
 parser.add_argument("url1")
 parser.add_argument("url2")
-args = parser.parse_args()
-t1 = get_text(args.url1)
-t2 = get_text(args.url2)
-f1 = word_freq(t1)
-f2 = word_freq(t2)
-s1 = simhash(f1)
-s2 = simhash(f2)
-print("The Common bits are:", common_bits(s1, s2), "/ 64")
+args=parser.parse_args()
+t1=get_text(args.url1)
+t2=get_text(args.url2)
+freq1=build_frequency(t1)
+freq2=build_frequency(t2)
+sm1=simhash(freq1)
+sm2=simhash(freq2)
+print("The Common bits are:",common_bits(sm1,sm2),"/ 64")
